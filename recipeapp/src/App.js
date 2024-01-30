4c63fbddc7716504de0d8b33f3ce095980ecb85d
athenabai25 <149978033+athenabai25@users.noreply.github.com>
2024-01-30 09:20:35 +0000

    Added cheesecake recipe & changed color of landing page

--- recipeapp/src/App.js
+++ recipeapp/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Accordion from 'react-bootstrap/Accordion';
 import './App.css';
 
@@ -6,60 +6,50 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Recipe App</h1>
-        <BasicExample />
+        <h1>Best Recipes</h1>
+        <RecipeFromAthena />
       </header>
     </div>
   );
 }
 
-
-function BasicExample() {
+function RecipeFromAthena() {
   return (
     <Accordion defaultActiveKey="0">
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Salty Crispy Chicken (Salted Fried Chicken)</Accordion.Header>
+        <Accordion.Header>Cheesecake</Accordion.Header>
         <Accordion.Body>
         <div align="left">
-        Procedure:<br />
-                      1.Cut chicken into pieces. Mix seasonings with chicken. Marinate for at least an hour.<br />
-                      2.Combine ingredients for batter. Set aside.<br />
-                      3.Heat oil in a pan over medium heat. Dip marinated chicken pieces first in batter, then deep fry until golden brown and crisp. Drain excess oil on paper towels. Serve immediately.<br />
-        Ingredients:<br />
-                     500 grams chicken thighs<br />
-                     1 tablespoon salt<br />
-                     1 teaspoon ground white pepper<br />
-                     1 tablespoon cooking oil<br />
-                    For batter:<br />
-                     1 cup plain flour<br />
-                     1 egg<br />
-                     1 tablespoon water<br />
-          < img src="1.jpeg"width="300px" height="200px"></img>
-        </div>
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Mushroom soup</Accordion.Header>
-        <Accordion.Body>
-        <div align="left">
-        Procedure:<br />
-                      1.Melt 2 tablespoons of the butter in a large pot such as a Dutch oven over medium-high heat. Add the garlic and shallot, cook for 1 minute, until they begin to soften. Add the mushrooms and cook for about 3 minutes, until tender and browned. Transfer all the contents of the pot to a bowl.<br />
-                      2.Add the remaining 1 tablespoon of butter to the pot. Once melted, sprinkle in the flour and whisk it quickly into a paste. Reduce the heat to medium. Add the stock a little at a time, whisking out the clumps between each addition.<br />
-                      3.Increase the heat back to medium-high and allow the soup to simmer well for 3 minutes. Add the mushrooms back to the pot and continue to cook for 2 more minutes. The stock will thicken slightly to be somewhat creamy.<br />
-                      4.Let cook for 3 to 4 minutes, then ladle into bowls. Garnish with mushrooms and parsley, if desired.<br />
-                      3 tablespoons unsalted butter
-        Ingredients:<br />
-                      1.2 garlic cloves, minced<br />
-                      2.1 shallot, finely chopped<br />
-                      3.4 ounces crimini mushrooms, chopped<br />
-                      4.4 ounces white button mushrooms, chopped<br />
-                      5.2 tablespoons all-purpose flour<br />
-                      6.3 cups chicken stock<br />
-                      7.¼ teaspoon fine sea salt, or to taste<br />
-                      8.¼ teaspoon ground black pepper, or to taste<br />
-                      9.Sliced sautéed mushrooms for garnish, optional<br />
-                      10.Chopped parsley for garnish, optional<br />
-        < img src="2.jpeg"width="300px" height="200px"></img>
+          Description:<br />
+              The filling for this delicious, easy cheesecake recipe has just eight
+              simple ingredients, and the cake bakes in a mere 30 minutes.<br />
+          <br />
+          Ingredients:<br />
+              (Crust)<br />
+              1/2 cups (150g) graham cracker crumbs (about 10 whole crackers, crushed)<br />
+              1/4 cup (28g) confectioners' sugar<br />
+              6 tablespoons (85g) butter, melted <br />
+              1/8 teaspoon table salt<br />
+              (Filling)<br />
+              two 8-ounce packages (452g) cream cheese, at room temperature<br />
+              2/3 cup (133g) granulated sugar<br />
+              2 large eggs, at room temperature<br />
+              1 teaspoon vanilla extract<br />
+          <br />
+          Instructions:<br />
+              1. Preheat the oven to 350°F.<br />
+              2. To make the crust: Stir together all of the crust ingredients, 
+              mixing until thoroughly combined.<br />
+              3. Press the crumbs into the bottom and up the sides of your pie pan<br />
+              4. To make the filling: Mix together the room-temperature cream cheese and 
+              sugar until smooth. Mix in the eggs and vanilla, again mixing until smooth.<br />
+              5. Set the pie pan onto a baking sheet, if desired; this protects the bottom of 
+              the crust from any potential scorching. Pour the filling into the crust.<br />
+              6. To bake the cheesecake: Place the cheesecake in the oven. 
+              Bake it for 30 minutes.<br />
+              7. Once the cake is cool, refrigerate it, covered, 
+              until you're ready to serve it.<br />
+          <img src="cheesecake.jpg" width="300px" height="200px" alt=""></img>
         </div>
         </Accordion.Body>
       </Accordion.Item>
