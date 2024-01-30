992ff3cc553e09d62d181584b367abca6b4e6f76
athenabai25 <149978033+athenabai25@users.noreply.github.com>
2024-01-30 19:39:15 +0000

    Added unit test coverage for each React component

--- recipeapp/src/App.test.js
+++ recipeapp/src/App.test.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import RecipeFromAthena from './App';
+import { render, screen } from '@testing-library/react';
 
 // Basic "smoke test" to check if
-// the component renders without throwing
+// the <App /> component renders without throwing
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+// "Smoke test" to check if
+// the <RecipeFromAthena /> component renders without throwing
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<RecipeFromAthena />, div);
+});
+
+// Test that the <App /> component renders
+// the heaing of the landing page "Best Recipes".
+it('renders landing page title', () => {
+  render(<App />);
+  expect(screen.getByText('Best Recipes')).toBeInTheDocument();
+});
+
+// Test that the <RecipeFromAthena /> component
+// renders the recipe name "Cheesecake".
+it('renders recipe name', () => {
+  render(<RecipeFromAthena />);
+  expect(screen.getByText('Cheesecake')).toBeInTheDocument();
+});
+
+// Test that the <RecipeFromAthena /> component renders
+// the image of the cheesecake 
+it('renders recipe image', () => {
+  render(<RecipeFromAthena />);
+  expect(screen.getByText('Best Recipes')).toBeInTheDocument();
 });
