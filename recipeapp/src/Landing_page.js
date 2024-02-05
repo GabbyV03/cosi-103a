92089401a4c888e4a0916427dcc47cced937b290
GabbyV03 <gvukomanovic@brandeis.edu>
2024-02-05 15:09:56 -0500

    temp bug fix

--- recipeapp/src/Landing_page.js
+++ recipeapp/src/Landing_page.js
@@ -3,7 +3,6 @@ import App from './App';
 import Team from './Team';
 import Button from 'react-bootstrap/Button';
 import React from 'react';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
@@ -20,68 +19,12 @@ import {
 // In the NavBar, the button with dropdown and the disabled button
 // are also not implemented and kept in the comments for reference.
 
-function RecipeSummary({ title, image }) {
-  return (
-    <div style={{ display: 'inline-block', margin: '10px' }}>
-      <Card style={{ width: '10rem' }}>
-      <Card.Img variant="top" src={image} alt={title} style={{ height: '200px', objectFit: 'cover' }} />
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-        </Card.Body>
-      </Card>
-    </div>
-  );
-}
 
 
 function Landing_page() {
-  const recipes = [
-    {
-      title: "Salty Crispy Chicken",
-      image: "1.jpeg"
-    },
-    {
-      title: "Mushroom soup",
-      image: "2.jpeg"
-    },
-    {
-      title: "Gyudon (Japanese Beef Bowl)",
-      image: "3.jpeg"
-    },
-    {
-      title: "Grilled Salmon with Lemon-Dill Sauce",
-      image: "4.jpeg"
-    },
-    {
-      title: "Chocolate Chip Cookies",
-      image: "cookie.png"
-    },
-    {
-      title: "Lemon Drizzle Cake",
-      image: "lemon_drizzle_cake.png"
-    },
-    {
-      title: "Cheesecake",
-      image: "cheesecake.jpg"
-    }
-  ];
   return (
     <div className="Landing_page">
       <Navigater />
-      <h2>Welcome!</h2>
-      <p>Here are some of our best recipes</p>
-      <Container fluid>
-        <div className="recipe-summary">
-          {recipes.map((recipe, index) => (
-            <RecipeSummary
-              key={index}
-              title={recipe.title}
-              image={recipe.image}
-            />
-          ))}
-        </div>
-      </Container>
-      <p>Click the link below to find out more!</p>
       <RouterProvider router={router}/>
     </div>
   );
