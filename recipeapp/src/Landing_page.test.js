43f73cf9ad08509a532bafe331640d7f4fecfcdf
Yao Sun <130551080+YaoSun0422@users.noreply.github.com>
2024-02-13 13:51:42 -0500

    Revert "added Groceryoffcanvas tests and updated Landing_page tests"

--- recipeapp/src/Landing_page.test.js
+++ recipeapp/src/Landing_page.test.js
@@ -1,33 +1,20 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Landing_page from './Landing_page';
+import BasicExamples from './Landing_page';
+import Navigater from './Landing_page';
 
-window.matchMedia = window.matchMedia || function() {
-  return {
-      matches : false,
-      addListener : function() {},
-      removeListener: function() {}
-  };
-};
-
-describe('Landing_page', () => {
-  test('renders Landing_page component without crashing', () => {
-    const mockProps = {
-      glist: [],
-      rtnGlist: jest.fn(),
-    };
-
-    render(<Landing_page {...mockProps} />);
+describe('BasicExamples', () => {
+  test('renders BasicExamples component', () => {
+    render(<BasicExamples />);
+    expect(screen.getByText('Salty Crispy Chicken, Gyudon, Cheesecake, and more!')).toBeInTheDocument();
+    expect(screen.getByText('Click the link below to view the recipes for these delicious dishes!')).toBeInTheDocument();
+    expect(screen.getByText('View Recipe')).toBeInTheDocument();
   });
+});
 
-  test('renders header', () => {
-    const mockProps = {
-      glist: [],
-      rtnGlist: jest.fn(),
-    };
-
-    render(<Landing_page {...mockProps} />);
-    const headerElement = screen.getByText(/Welcome to the COSI-103A Group T project page!/i);
-    expect(headerElement).toBeInTheDocument();
+describe('Navigater', () => {
+  test('renders Navigater component', () => {
+    render(<Navigater />);
+    expect(screen.getByText('Group T')).toBeInTheDocument();
   });
-
 });
