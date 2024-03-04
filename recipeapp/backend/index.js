75b4369fb70c4764fd7f5b3ea358aeec2e1c3998
GabbyV03 <gvukomanovic@brandeis.edu>
2024-03-04 02:32:56 -0500

    fixed ingredients in recipe api, TO BE ADDED-INSTRUCTIONS AND IMAGE FUNCTIONALITY(?)

--- recipeapp/backend/index.js
+++ recipeapp/backend/index.js
@@ -13,37 +13,37 @@ let recipes = [
   { 
     id: 2, 
     name: 'Mushroom Soup', 
-    ingredients: ['lettuce', 'tomato', 'cucumber'], 
+    ingredients: ['3 tablespoons unsalted butter', '2 garlic cloves', '1 shallot', '4 ounces crimini mushrooms', '4 ounces white button mushrooms', '2 tablespoons all-purpose flour', '3 cups chicken stock', '1/4 teaspoon fine sea salt', '1/4 teaspoon ground black pepper'], 
     steps: ['Chop lettuce', 'Slice tomato', 'Dice cucumber'] 
   },
   { 
     id: 3, 
     name: 'Gyudon', 
-    ingredients: ['lettuce', 'tomato', 'cucumber'], 
+    ingredients: ['300 grams thinly sliced beef', '1 onion', '1 cup dashi stock', '3 tablespoons soy sauce', '3 tablespoons mirin', '2 tablespoons sugar'], 
     steps: ['Chop lettuce', 'Slice tomato', 'Dice cucumber'] 
   },
   { 
     id: 4, 
     name: 'Grilled Salmon with Lemon-Dill Sauce', 
-    ingredients: ['lettuce', 'tomato', 'cucumber'], 
+    ingredients: ['4 salmon fillets', 'salt and pepper', '1/4 cup lemon juice', '2 tablespoons olive oil', '2 cloves garlic', '2 tablespoons fresh dill', '1 tablespoon honey'], 
     steps: ['Chop lettuce', 'Slice tomato', 'Dice cucumber'] 
   },
   { 
     id: 5, 
     name: 'Chocolate Chip Cookie', 
-    ingredients: ['lettuce', 'tomato', 'cucumber'], 
+    ingredients: ['1 cup unsalted butter', '3/4 cup granulated sugar', '3/4 cup packed brown sugar', '1 teaspoon vanilla extract', '2 large eggs', '2 1/4 cups all-purpose flour', '1 teaspoon baking soda', '2 cups semisweet chocolate chips'], 
     steps: ['Chop lettuce', 'Slice tomato', 'Dice cucumber'] 
   },
   { 
     id: 6, 
     name: 'Lemon Drizzle Cake', 
-    ingredients: ['lettuce', 'tomato', 'cucumber'], 
+    ingredients: ['225 grams unsalted butter', '225 graams caster sugar', '4 large eggs', '225 grams self-raising flour', '1 teaspoon vanilla extract'],  
     steps: ['Chop lettuce', 'Slice tomato', 'Dice cucumber'] 
   },
   { 
     id: 7, 
     name: 'Cheesecake', 
-    ingredients: ['lettuce', 'tomato', 'cucumber'], 
+    ingredients: ['1/2 cup graham cracker crumbs', '1/4 cup confectioners sugar', '6 tablespoons unsalted butter', '1/8 teaspoon table salt', '16 ounces cream cheese', '2 large eggs', '2/3 cup granulated sugar', '1 teaspoon vanilla extract'], 
     steps: ['Chop lettuce', 'Slice tomato', 'Dice cucumber'] 
   },
 ];
