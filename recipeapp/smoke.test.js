b5192e80bc781a66e29dbe8800e21aa306e9db40
athenabai25 <149978033+athenabai25@users.noreply.github.com>
2024-04-08 18:28:37 +0000

    Added a basic smoke test for the recipe page

--- recipeapp/smoke.test.js
+++ recipeapp/smoke.test.js
@@ -1,2 +1,14 @@
-//Just for testing workflow file
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+
+// Test recipe page
+test('renders app without crashing', () => {
+  render(<App />);
+});
+
+// Can do: test other pages 
+
+// Just for testing workflow file
 process.exit(1);
+
