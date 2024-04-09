8f7268fc7ac3f78579df4c3eb676a138bd22cbbe
Yao Sun <130551080+YaoSun0422@users.noreply.github.com>
2024-04-09 01:46:01 +0000

    first try

--- recipeapp/smoke.test.js
+++ recipeapp/smoke.test.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-// Test the recipe page
+// Smoke test for the Landing Page
 test('renders app without crashing', () => {
   render(<App />);
 });
+
+// Smoke test for the Team Page
+test('renders Team Page', () => {
+  render(<TeamPage />);
+  const headerElement = screen.getByRole('heading');
+  expect(headerElement).toBeInTheDocument();
+});
+
 
 // Smoke test for the "Add New Recipe" button  
 test('adds a new recipe into the list', async () => {
@@ -23,9 +31,20 @@ test('adds a new recipe into the list', async () => {
   });
 });
 
-// Can do: test landing page, team page,
-// and/or edition/deletion buttons on the recipe page
 
+// and/or edition/deletion buttons on the recipe page
+test('renders deletion button and checks its functionality', async () => {
+  render(<RecipePage />);
+  const deleteButton = screen.getByRole('button', { name: /delete/i });
+  userEvent.click(deleteButton);
+
+  // Confirm the deletion
+  const confirmButton = screen.getByRole('button', { name: /OK/i });
+  userEvent.click(confirmButton);
+
+  // Wait for the recipes to be updated
+  await screen.findByText('Recipe deleted successfully');
+});
 
 // Just for testing workflow file
 process.exit(1);
